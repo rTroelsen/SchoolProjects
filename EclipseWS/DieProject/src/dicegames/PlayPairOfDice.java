0f9b07fc6183837862eafb1644582fb5b8b97b27
Rasmus Troelsen <rasmus8850@gmail.com>
2016-09-26 19:17:03 +0200

    Updated

--- EclipseWS/DieProject/src/dicegames/PlayPairOfDice.java
+++ EclipseWS/DieProject/src/dicegames/PlayPairOfDice.java
@@ -3,22 +3,22 @@ package dicegames;
 import java.util.Scanner;
 
 public class PlayPairOfDice {
-    
+
     /**
      * How many rolls have been rolled.
      */
     private int rolls;
-    
+
     /**
      * The scanner used for reading user input.
      */
     private Scanner scan;
-    
+
     /**
      * The die used in the game.
      */
     private PairOfDices dices;
-    
+
     /**
      * Constructs the PlayPairOfDice game.
      */
@@ -26,14 +26,14 @@ public class PlayPairOfDice {
         dices = new PairOfDices();
         scan = new Scanner(System.in);
     }
-    
+
     /**
      * Prints a welcoming message.
      */
     private void welcomeMessage() {
         System.out.println("Welcome to the greatest dice game!");
     }
-
+    
     /**
      * Ends the game, telling you:
      *
@@ -53,15 +53,15 @@ public class PlayPairOfDice {
         System.out.println("You have rolled " + rolls + " times!");
         System.out.println("You've hit doubles " + dices.getSameValue() + " times!");
         System.out.println("Your highest roll was: " + dices.getHighestRoll() + "!");
-        System.out.println("You have rolled " + dices.getSix() + " sixes!");
-        System.out.println("You have rolled " + dices.getFive() + " fives!");
-        System.out.println("You have rolled " + dices.getFour() + " fours!");
-        System.out.println("You have rolled " + dices.getThree() + " threes!");
-        System.out.println("You have rolled " + dices.getTwo() + " twos!");
-        System.out.println("You have rolled " + dices.getOne() + " ones!");
+        System.out.println("You have rolled " + dices.getSixes() + " sixes!");
+        System.out.println("You have rolled " + dices.getFives() + " fives!");
+        System.out.println("You have rolled " + dices.getFours() + " fours!");
+        System.out.println("You have rolled " + dices.getThrees() + " threes!");
+        System.out.println("You have rolled " + dices.getTwos() + " twos!");
+        System.out.println("You have rolled " + dices.getOnes() + " ones!");
         System.out.println("Thanks for playing, please come again!");
     }
-    
+
     /**
      * Rolls both dices and adds one to the 'rolls' counter.
      * Then prints the sum of the roll
@@ -69,9 +69,10 @@ public class PlayPairOfDice {
     public void takeTurn() {
         dices.rollBothDices();
         rolls++;
-        System.out.println("Your roll is: " + dices.sumOfDices() + "!");
+        System.out.println("Your roll is: " + dices.sumOfDices() + " (" + dices.getFirstFaceValue()
+            + " and " + dices.getSecondFaceValue() + ")!");
     }
-
+    
     /**
      * Starts the game.
      * While the boolean 'gameOver' is false, the game is running.
@@ -79,16 +80,16 @@ public class PlayPairOfDice {
      */
     public void startGame() {
         welcomeMessage();
-        
-        boolean gameOver = false;
 
+        boolean gameOver = false;
+        
         // this while loop keeps the game running.
         while (!gameOver) {
             System.out.println("Throw dices? Y/N");
             String proceedWithGame = scan.nextLine();
-
+            
             // if the game is over change gameOver to true. Sending us to endMessage()
-            if (proceedWithGame.equalsIgnoreCase("N")) {
+            if (proceedWithGame.equalsIgnoreCase("n")) {
                 gameOver = true;
             }
             else { // if the game is not over, take a new turn!
@@ -97,5 +98,5 @@ public class PlayPairOfDice {
         }
         endMessage();
     }
-    
+
 }
