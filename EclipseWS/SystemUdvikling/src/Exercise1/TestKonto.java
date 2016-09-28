568c032e7531b0dd78154e65c973461e88ebf977
rTroelsen <rasmus8850@gmail.com>
2016-09-28 12:04:58 +0200

    SystemUdvikling Tilføjet

---
+++ EclipseWS/SystemUdvikling/src/Exercise1/TestKonto.java
@@ -0,0 +1,10 @@
+package Exercise1;
+
+public class TestKonto {
+    
+    public static void main(String[] args) {
+        Konto konto = new Konto("hestePik", 69);
+        System.out.println(konto.getNr());
+    }
+    
+}
